f35c1b8c8f2fd9dba79454b4500e51bc2734856f
Trung Nghia <nh0cbum1234@gmail.com>
2025-06-29 03:22:07 +0700

    tt

--- live-code-editor.js
+++ live-code-editor.js
@@ -7,6 +7,7 @@ codeEditor.oninput = function (event) {
     previewFrame.setAttribute("srcdoc", this.value);
 };
 window.onbeforeunload = function (e) {
+    e.preventDefault();
     return "Bạn có chắc chắn muốn rời khỏi trang này không? Dữ liệu chưa được lưu có thể bị mất.";
 };
 
